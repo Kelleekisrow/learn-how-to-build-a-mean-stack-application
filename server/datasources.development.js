96b8375edfe321f5fa3260dba22267ec9e7bbf7f
Kelleekisrow <125936482+Kelleekisrow@users.noreply.github.com>
2024-09-14 14:59:43 -0700

    Update datasources.development.js

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -5,7 +5,7 @@ module.exports = {
    port: process.env.DB_PORT || 27017,
    user: process.env.DB_USER || '',
    password: process.env.DB_PASSWORD || '',
-   database: process.env.DB_NAME || 'lafs-db',
+   database: process.env.DB_NAME || 'lafs',
    url: process.env.DB_URL
  }
 };
